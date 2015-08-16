8952fd2c4aac27e994b7951747ac21d600fff15f
Brandon Smith <vectorcell@gmail.com>
2015-08-16 03:08:22 -0500

    refined install/uninstall options

--- install.sh
+++ install.sh
@@ -3,13 +3,21 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 dir=$(dirname $(readlink -f $DIR/$(basename "$0")))
 
-bindir="$HOME/bin"
+if [ "$(whoami)" == root ]; then
+	bindir="/usr/local/bin"
+else
+	bindir="$HOME/bin"
 cd $bindir
 
 for file in $(ls $dir | grep pass | grep -v ".c"); do
 	src="$dir/$file"
 	dest="$bindir/$file"
-	echo ln -s $src $dest
-	ln -s $src $dest
+	if [ "$(whoami)" == root ]; then
+		echo cp $src $dest
+		cp $src $dest
+	else
+		echo ln -s $src $dest
+		ln -s $src $dest
+	fi
 done
 
--- uninstall.sh
+++ uninstall.sh
@@ -3,14 +3,17 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 dir=$(dirname $(readlink -f $DIR/$(basename "$0")))
 
-bindir="$HOME/bin"
+if [ "$(whoami)" == root ]; then
+	bindir="/usr/local/bin"
+else
+	bindir="$HOME/bin"
 cd $bindir
 
 for file in $(ls $dir | grep pass | grep -v ".c"); do
 	installed="$bindir/$file"
 	if [ -e $installed ]; then
-		echo rm -f $installed
-		rm -f $installed
+		echo rm $installed
+		rm $installed
 	fi
 done
 
