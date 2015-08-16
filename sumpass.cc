0cc3113d1e46d75bbed81d25fae5075dbf36b31d
Brandon Smith <vectorcell@gmail.com>
2015-08-16 12:37:09 -0500

    moved digest function declarations/implementations to header file

--- sumpass.cc
+++ sumpass.cc
@@ -12,30 +12,6 @@
 
 using namespace std;
 
-const string md5_digest (FILE* infile, FILE* outfile) {
-	return openssl_digest(infile, outfile, MD5_DIGEST_LENGTH, MD5_Init, MD5_Update, MD5_Final);
-}
-
-const string sha1_digest (FILE* infile, FILE* outfile) {
-	return openssl_digest(infile, outfile, SHA_DIGEST_LENGTH, SHA1_Init, SHA1_Update, SHA1_Final);
-}
-
-const string sha224_digest (FILE* infile, FILE* outfile) {
-	return openssl_digest(infile, outfile, SHA224_DIGEST_LENGTH, SHA224_Init, SHA224_Update, SHA224_Final);
-}
-
-const string sha256_digest (FILE* infile, FILE* outfile) {
-	return openssl_digest(infile, outfile, SHA256_DIGEST_LENGTH, SHA256_Init, SHA256_Update, SHA256_Final);
-}
-
-const string sha384_digest (FILE* infile, FILE* outfile) {
-	return openssl_digest(infile, outfile, SHA384_DIGEST_LENGTH, SHA384_Init, SHA384_Update, SHA384_Final);
-}
-
-const string sha512_digest (FILE* infile, FILE* outfile) {
-	return openssl_digest(infile, outfile, SHA512_DIGEST_LENGTH, SHA512_Init, SHA512_Update, SHA512_Final);
-}
-
 typedef function<string(FILE*, FILE*)> digest_fn;
 
 int main (int argc, char *argv[]) {
--- sumpass.h
+++ sumpass.h
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <string>
 #include <vector>
+
 #include <openssl/md5.h>
 #include <openssl/sha.h>
 
@@ -34,4 +35,28 @@ const std::string openssl_digest (FILE* infile, FILE* outfile, size_t digest_len
 	}
 	free(digest);
 	return std::string(out.begin(), out.end());
+}
+
+const std::string md5_digest (FILE* infile, FILE* outfile) {
+	return openssl_digest(infile, outfile, MD5_DIGEST_LENGTH, MD5_Init, MD5_Update, MD5_Final);
+}
+
+const std::string sha1_digest (FILE* infile, FILE* outfile) {
+	return openssl_digest(infile, outfile, SHA_DIGEST_LENGTH, SHA1_Init, SHA1_Update, SHA1_Final);
+}
+
+const std::string sha224_digest (FILE* infile, FILE* outfile) {
+	return openssl_digest(infile, outfile, SHA224_DIGEST_LENGTH, SHA224_Init, SHA224_Update, SHA224_Final);
+}
+
+const std::string sha256_digest (FILE* infile, FILE* outfile) {
+	return openssl_digest(infile, outfile, SHA256_DIGEST_LENGTH, SHA256_Init, SHA256_Update, SHA256_Final);
+}
+
+const std::string sha384_digest (FILE* infile, FILE* outfile) {
+	return openssl_digest(infile, outfile, SHA384_DIGEST_LENGTH, SHA384_Init, SHA384_Update, SHA384_Final);
+}
+
+const std::string sha512_digest (FILE* infile, FILE* outfile) {
+	return openssl_digest(infile, outfile, SHA512_DIGEST_LENGTH, SHA512_Init, SHA512_Update, SHA512_Final);
 }
