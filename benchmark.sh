8f920e5add3792fb7f199bd6d49f9075995397ed
Brandon Smith <vectorcell@gmail.com>
2015-08-16 00:17:03 -0500

    added benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ export PATH=.:$PATH
 
 HASHES="md5 sha1 sha224 sha256 sha384 sha512"
 
-TEMPFILE="/run/shm/sparse-$RANDOM.file"
+TEMPFILE="sparse-$RANDOM.file"
 dd if=/dev/zero of=$TEMPFILE bs=1k count=0 seek=1M 2> /dev/null
 
 for HASH in $(echo $HASHES | tr ' ' '\n'); do
